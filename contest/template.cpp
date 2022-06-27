ed5307bf9f6d245d3206984d8dfe84aaa0bcc0e2
Bing-Dong Liu <107341533+bingdong-liu@users.noreply.github.com>
2022-06-27 10:47:44 -0700

    Create template.cpp

---
+++ contest/template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(v) static_cast<int>(v.size())
+#define long long long
+
+const bool IO = []() {
+    #ifndef LOCAL
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        return true;
+    #else
+        return false;
+    #endif
+}();
+
+#ifdef LOCAL
+    #define dbg(...) printf("\033[1;34m% 4d \033[32m|\033[0m ", __LINE__), printf(__VA_ARGS__)
+#else
+    #define dbg(...) 0
+#endif
+
+int main() {
+    
+    return 0;
+}
