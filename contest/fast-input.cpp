65e02b836f75bd1c98d87ef6a33cff2ced7d6411
Bing-Dong Liu <107341533+bingdong-liu@users.noreply.github.com>
2022-07-02 12:50:47 -0700

    Update fast-input.cpp

--- contest/fast-input.cpp
+++ contest/fast-input.cpp
@@ -1,12 +1,28 @@
 inline namespace input {
+  inline char get_char() {
+    #ifdef LOCAL
+      return getchar();
+    #else
+      static const int SIZE = 1 << 20;
+      static char buf[SIZE], *b = buf;
+      static int cnt = 0;
+
+      if (cnt == 0) {
+        if ((cnt = fread(buf, 1, SIZE, stdin)) == 0)
+          return EOF;
+        b = buf;
+      }
+      return cnt--, *b++;
+    #endif
+  }
   inline void rd(string& s) {
-    char c = getchar();
+    char c = get_char();
 
     while (c <= ' ')
       ;
     s.clear();
     while (c > ' ')
-      s += c, c = getchar();
+      s += c, c = get_char();
   }
   template<class T>
   inline typename enable_if<is_floating_point<T>::value, void>::type rd(T& x) {
@@ -17,14 +33,14 @@ inline namespace input {
   }
   template<class T>
   inline typename enable_if<is_integral<T>::value, void>::type rd(T& x) {
-    char c = getchar();
+    char c = get_char();
     bool neg = false;
 
     x = 0;
     while (c < '0' || c > '9')
-      neg |= c == '-', c = getchar();
+      neg |= c == '-', c = get_char();
     while (c >= '0' && c <= '9')
-      x = (x << 3) + (x << 1) + (c ^ 48), c = getchar();
+      x = (x << 3) + (x << 1) + (c ^ 48), c = get_char();
     if (neg)
       x = -x;
   }
