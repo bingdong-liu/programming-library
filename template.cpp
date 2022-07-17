84c638b94b220b3ff7a979d00e990600d0ccd04e
bingdong-liu <107341533+bingdong-liu@users.noreply.github.com>
2022-07-17 15:29:27 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,17 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef LOCAL
-  #define dbg(...) printf("\033[1;34m% 4d \033[32m|\033[0m ", __LINE__), printf(__VA_ARGS__)
-#else
-  #define dbg(...) 0
-#endif
-
 int main() {
-#ifndef LOCAL
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-#endif
   
   return 0;
 }
