f29b3e14f934b4aff218bbb4147335d8c42c1572
bingdong-liu <107341533+bingdong-liu@users.noreply.github.com>
2022-07-24 15:47:33 -0700

    Update line-container.cpp

--- data-structure/line-container.cpp
+++ data-structure/line-container.cpp
@@ -3,51 +3,44 @@ line container
 ---
 add lines and query max value for given x
 */
-template<class T>
+
 struct line {
-  mutable T k, m, p;
+  mutable long long k, m, p;
 
   bool operator<(const line& l) const {
     return k < l.k;
   }
-  bool operator<(T x) const {
+  bool operator<(long long x) const {
     return p < x;
   }
-  T operator()(const T& x) const {
+  long long operator()(const long long& x) const {
     return k * x + m;
   }
 };
 
-template<class T>
-struct line_container {
-  static const T inf = numeric_limits<T>::max();
-  multiset<line<T>, less<>> cc;
-
-  T div(T a, T b) {
+struct line_container : multiset<line, less<>> {
+  long long div(long long a, long long b) {
     return a / b - ((a ^ b) < 0 && a % b); }
-  template<class I>
-  bool isect(I x, I y) {
-    if (y == cc.end())
-      return x->p = inf, 0;
+  bool isect(iterator x, iterator y) {
+    if (y == end())
+      return x->p = LLONG_MAX, 0;
     if (x->k == y->k)
-      x->p = x->m > y->m ? inf : -inf;
+      x->p = x->m > y->m ? LLONG_MAX : -LLONG_MAX;
     else
       x->p = div(y->m - x->m, x->k - y->k);
     return x->p >= y->p;
   }
-  void add(T k, T m) {
-    auto z = cc.insert({k, m, 0}), y = z++, x = y;
-
+  void add(long long k, long long m) {
+    auto z = insert({k, m, 0}), y = z++, x = y;
     while (isect(y, z))
-      z = cc.erase(z);
-    if (x != cc.begin() && isect(--x, y))
-      isect(x, y = cc.erase(y));
-    while ((y = x) != cc.begin() && (--x)->p >= y->p)
-      isect(x, cc.erase(y));
+      z = erase(z);
+    if (x != begin() && isect(--x, y))
+      isect(x, y = erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p)
+      isect(x, erase(y));
   }
-  T query(T x) {
-    assert(!cc.empty());
-
-    return (*cc.lower_bound(x))(x);
+  long long query(long long x) {
+    assert(!empty());
+    return (*lower_bound(x))(x);
   }
 };
